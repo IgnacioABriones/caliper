cdc20f8d95dcccfcd62a60ad7627eac2054ee8e5
Luis Herrera <lherrera@gignosoft.cl>
2016-07-30 19:09:44 -0400

    se repara users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,19 +15,6 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-<<<<<<< HEAD
-<<<<<<< HEAD
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
-            $table->timestamps();
-=======
-=======
->>>>>>> c1850966f1bb5da9cfffe3f7e8c8213277b5b3d9
-            $table->string('identifier');
-            $table->string('annexed');
-            $table->string('photo_url');
-            $table->integer('city_id')->unsigned();
             $table->string('email')->unique();
             $table->string('password', 60);
             $table->rememberToken();
@@ -37,10 +24,7 @@ class CreateUsersTable extends Migration
                 ->references('id')
                 ->on('cities')
                 ->onDelete('cascade');
-<<<<<<< HEAD
->>>>>>> c1850966f1bb5da9cfffe3f7e8c8213277b5b3d9
-=======
->>>>>>> c1850966f1bb5da9cfffe3f7e8c8213277b5b3d9
+
         });
     }
 
@@ -51,14 +35,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        Schema::drop('users');
-=======
         Schema::dropIfExists('users');
->>>>>>> c1850966f1bb5da9cfffe3f7e8c8213277b5b3d9
-=======
-        Schema::dropIfExists('users');
->>>>>>> c1850966f1bb5da9cfffe3f7e8c8213277b5b3d9
     }
 }
